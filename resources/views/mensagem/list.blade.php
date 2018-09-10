b741c3d5e344acb2e051a1ecbcfc183875dca031
LinuxIfrs <victor.caraffini@ibiruba.ifrs.edu.br>
2018-09-10 14:54:37 -0300

    primeiro

---
+++ resources/views/mensagem/list.blade.php
@@ -0,0 +1,27 @@
+<h1>Lista de Mensagens</h1>
+<hr>
+ @if(\Session::has('success'))
+	<div class="container">
+  		<div class="alert alert-success">
+    		{{\Session::get('success')}}
+  		</div>
+  	</div>
+  @endif
+
+@foreach($mensagens as $mensagem)
+	<h3>{{$mensagem->autor}}</h3>
+	<p><a href="/mensagem/{{$mensagem->id}}">{{$mensagem->titulo}}</a></p>
+
+	<p>{{$mensagem->texto}}</p>
+	<p><a href="/mensagem/{{$mensagem->id}}/edit">Editar</a> </p>
+		<p><a href="/mensagem/{{$mensagem->id}}/delete">Deletar</a> </p>
+
+	<br>
+@endforeach
+
+<br>
+<p><a href="/mensagem/create">Criar novo registro</a></p>
+
+
+
+<!-- \Carbon\Carbon::parse($atividade->scheduledto)->format('d/m/Y h:m')  -->
